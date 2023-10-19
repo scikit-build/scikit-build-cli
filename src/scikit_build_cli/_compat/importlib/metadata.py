f3d777d9af551f66c8e341a9166342df3888d4d6
Henry Schreiner <henryschreineriii@gmail.com>
2023-10-19 10:05:38 -0400

    fix: get checks passing
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/scikit_build_cli/_compat/importlib/metadata.py
+++ src/scikit_build_cli/_compat/importlib/metadata.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
-import importlib.metadata as _metadata
+import importlib.metadata
 import sys
 
 __all__ = ["entry_points"]
 
 
-def entry_points(*, group: str) -> _metadata.EntryPoints:  # type: ignore[name-defined]
+def entry_points(*, group: str) -> importlib.metadata.EntryPoints:  # type: ignore[name-defined]
+    """
+    Wrapper for entry_points to support Python 3.8+ instead of 3.10+.
+    """
     if sys.version_info >= (3, 10):
-        return _metadata.entry_points(group=group)
+        return importlib.metadata.entry_points(group=group)
 
-    epg = _metadata.entry_points()
+    epg = importlib.metadata.entry_points()
 
     return epg.get(group, [])
 
